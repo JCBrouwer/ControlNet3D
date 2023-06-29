5decc5ec4cdbcc2b21c99ac9c47633c8f45961eb
Hans <hans@brouwer.work>
2023-06-29 19:39:43 +0200

    please ruff

--- pipeline_controlnet3d.py
+++ pipeline_controlnet3d.py
@@ -403,7 +403,8 @@ class TextToVideoControlNetPipeline(DiffusionPipeline, TextualInversionLoaderMix
         video = video.float()
         return video
 
-    # from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion.StableDiffusionPipeline.prepare_extra_step_kwargs
+    # from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion.StableDiffusionPipeline
+    #        .prepare_extra_step_kwargs
     def prepare_extra_step_kwargs(self, generator, eta):
         # prepare extra kwargs for the scheduler step, since not all schedulers have the same signature
         # eta (η) is only used with the DDIMScheduler, it will be ignored for other schedulers.
